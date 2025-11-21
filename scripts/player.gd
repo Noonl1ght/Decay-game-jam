f28804a23330b538d0ef44aee001a0fe88a360f2
Noonl1ght <vinanimated@gmail.com>
2025-11-21 08:34:13 +0530

    character controller update
    the camera no longer does cartwheels when mouse is moved to an extreme.
    
    the camera now has a head bob effect simulating a walking character

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,12 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.03
+const SENSITIVITY = 0.003
+
+#bob variables
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
 var gravity = 9.8
 
@@ -16,6 +21,8 @@ func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		camera.rotation.z = clamp(camera.rotation.z, deg_to_rad(-40), deg_to_rad(60))
 
 func _physics_process(delta):
 	
@@ -26,7 +33,7 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 	
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -34,4 +41,14 @@ func _physics_process(delta):
 		velocity.x = 0.0
 		velocity.z = 0.0
 	
+	#head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
