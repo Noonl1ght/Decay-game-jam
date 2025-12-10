2ce70ecf68f5ab8eaf64b54cc36d932dcc8c15fb
Noonl1ght <vinanimated@gmail.com>
2025-12-10 21:37:41 +0530

    changes
    reach added to character
    collision body added to barrel

--- scripts/player.gd
+++ scripts/player.gd
@@ -31,6 +31,15 @@ func _unhandled_input(event):
 
 func _physics_process(delta):
 	
+	$CanvasLayer/BoxContainer/InteractText.hide()
+	if %Reach.is_colliding():
+		var target = %Reach.get_collider()
+		if target.has_method("interact"):
+			$CanvasLayer/BoxContainer/InteractText.show()
+			print("you can interact with this item!")
+			if Input.is_action_just_pressed("interact"):
+				target.interact()
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
